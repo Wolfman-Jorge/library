54c3fc69297c9ac8295077921ad7928be9934d80
Jorge Dominguez <jorgeadominguezgonzalez@gmail.com>
2025-01-29 17:43:07 +0100

    descomentado codigo warnings

--- src/main/java/library/library/persistence/entity/RoleEntity.java
+++ src/main/java/library/library/persistence/entity/RoleEntity.java
@@ -1,42 +1,42 @@
-//package library.library.persistence.entity;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Builder;
-//
-//@Builder
-//@Entity
-//@Table(name = "role")
-//public class RoleEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Enumerated(EnumType.STRING)
-//    private RoleEnum name;
-//
-//    public RoleEntity() {
-//    }
-//
-//    public RoleEntity(Long id, RoleEnum name) {
-//        this.id = id;
-//        this.name = name;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public RoleEnum getName() {
-//        return name;
-//    }
-//
-//    public void setName(RoleEnum name) {
-//        this.name = name;
-//    }
-//}
+package library.library.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Builder
+@Entity
+@Table(name = "role")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleEnum name;
+
+    public RoleEntity() {
+    }
+
+    public RoleEntity(Long id, RoleEnum name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleEnum getName() {
+        return name;
+    }
+
+    public void setName(RoleEnum name) {
+        this.name = name;
+    }
+}
